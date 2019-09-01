8e9522ed6911c137b819cd8ec8d77cba9b4e59b7
Lihyrs <llg4192@126.com>
2019-09-02 00:40:06 +0800

    feat(store):添加state及相关方法

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,46 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    activeId: '',
+    articlesSet: new Map(),
+    curArticles: {},
   },
   mutations: {
-
+    articles: (state, payload) => {
+      const { articles, channelId } = payload;
+      if (articles) {
+        state.articlesSet.set(channelId, articles);
+      }
+    },
+    activeId: (state, payload) => {
+      const { activeId } = payload;
+      if (!activeId) return;
+      state.activeId = activeId;
+    },
+    curArticles: (state, payload) => {
+      const { activeId } = payload;
+      state.curArticles = state.articlesSet.get(activeId);
+    },
   },
   actions: {
-
+    updateArticles: ({ commit }, payload) => {
+      commit('articles', payload);
+    },
+    updateActiveId: ({ commit }, payload) => {
+      commit('activeId', payload);
+    },
+    updateCurArticles: ({ commit }, payload) => {
+      commit('curArticles', payload);
+    },
+    changeActive: ({ dispatch }, payload) => {
+      dispatch('updateActiveId', payload);
+      dispatch('updateCurArticles', payload);
+    },
+  },
+  getters: {
+    getArticlesByChannelId: state => id => state.articlesSet.get(id),
+    getActiveId: state => state.activeId,
+    getCurArticles: state => state.curArticles,
+    getArticlesSet: state => state.articlesSet,
   },
 });
