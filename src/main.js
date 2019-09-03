5089bb70f79173706b92e12b136d459800e0cb17
Lihyrs <llg4192@126.com>
2019-09-03 08:09:43 +0800

    style(main.js):去除错误的注释

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue';
 import '@vant/touch-emulator';
 import App from './components/AppLayout.vue';
-// import router from './router';
-// import store from './store';
+import router from './router';
+import store from './store';
 import './utils/globalComponents';
+import $fetch from './plugins/fetch';
 
 import db from './plugins/db';
 
 Vue.config.productionTip = false;
 
 Vue.use(db);
+Vue.use($fetch);
 
 new Vue({
-  // router,
-  // store,
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app');
