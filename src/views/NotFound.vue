7c18ad6941f229fa923cabcd19bcc1772782012e
Lihyrs <llg4192@126.com>
2019-09-05 17:05:07 +0800

    feat(404page):修改404页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>404</div>
+  <div class="main">
+    <img class="img404" src="../assets/imgs/404img.png" alt="404" />
+    <div>
+      <router-link :to="{name:'home'}">
+        <van-button type="danger">返回首页</van-button>
+      </router-link>
+    </div>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.main {
+  width: 100%;
+  min-height: 233px;
+  text-align: center;
+
+  .img404 {
+    padding-top: 68px;
+    padding-bottom: 48px;
+    width: 282px;
+    height: 218px;
+  }
+}
+</style>
